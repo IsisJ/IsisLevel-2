46e792c0d60ee95fc164085a9107c77819e63d88
Kendra Kleber <league@Mac-mini-2.local>
2018-04-14 12:13:13 -0700

    warm up

---
+++ src/org/jointheleague/WarmUp2.java
@@ -0,0 +1,15 @@
+package org.jointheleague;
+
+import java.util.ArrayList;
+
+public class WarmUp2 {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("John");
+		names.add("Isis");
+		names.set(0, "Esme");
+		System.out.println(names);
+	}
+}
