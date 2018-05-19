3086d5d961fef46c32b5e6b7a74bbf08e1dd6a25
tactreconx <league@Admins-Mac-mini-2.local>
2018-05-19 11:37:27 -0700

    You can now switch between states!
    Congrats!

--- src/ChooseAGameName/GamePanel.java
+++ src/ChooseAGameName/GamePanel.java
@@ -11,23 +11,33 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener {
 
-	// graphics and layout for each page ( game page, and map); maybe handle mouse clicks(for map)
-	
-	//JTextArea storyArea = new JTextArea("hey", 1, 1);
-	JTextArea storyArea = new JTextArea(">Hello, how are you doing today?!\n\n\n\n\n\n\n\nsome text\n\n\n\n\n\n\n\n even more text", 20,30);
+	// graphics and layout for each page ( game page, and map); maybe handle mouse
+	// clicks(for map)
+
+	// JTextArea storyArea = new JTextArea("hey", 1, 1);
+	JTextArea storyArea = new JTextArea(
+			">Hello, how are you doing today?!\n\n\n\n\n\n\n\nsome text\n\n\n\n\n\n\n\n even more text", 20, 30);
 	JLabel deathCount = new JLabel("Deaths: put num later");
 	JScrollPane scroll = new JScrollPane(storyArea);
 	JButton mapButton = new JButton();
 	JLabel map = new JLabel("You're in the map");
+
 	int storyState = 1;
 	int mapState = 2;
 	int currentState = storyState;
 
-	
-	
+	Timer timer = new Timer(1000, new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.println(currentState);
+		}
+
+	});
 
 	public void GamePage() {
 		// panel= new GridLayout(3,1);
@@ -40,9 +50,8 @@ public class GamePanel extends JPanel implements ActionListener {
 		this.add(mapButton);
 		mapButton.setText("Map");
 		mapButton.addActionListener(this);
-
+		timer.start();		
 	}
-	
 
 	public void Map() {
 
@@ -51,45 +60,45 @@ public class GamePanel extends JPanel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton buttonPressed = (JButton) e.getSource();
-		if(buttonPressed == mapButton){
-			//this.ShowMap();			
-			if(currentState == mapState ){
+		if (buttonPressed == mapButton) {
+			// this.ShowMap();
+			if (currentState == mapState) {
 				this.HideMap();
 				this.ShowStory();
+				currentState = storyState;
 			}
-			if(currentState == storyState){
+			else if (currentState == storyState) {
 				this.HideStory();
 				this.ShowMap();
+				currentState=mapState;
 			}
 		}
 	}
 
-	public void ShowMap(){
-		
+	public void ShowMap() {
+
 		storyArea.setVisible(false);
 		deathCount.setVisible(false);
 		scroll.setVisible(false);
 		this.add(map);
-		currentState = mapState;
+		
 	}
-	
-	public void ShowStory(){
+
+	public void ShowStory() {
 		storyArea.setVisible(true);
 		deathCount.setVisible(true);
-		scroll.setVisible(true);	
-		currentState = storyState;
+		scroll.setVisible(true);
 	}
-	
-	public void HideMap(){
+
+	public void HideMap() {
 		map.setVisible(false);
 	}
-	
-	public void HideStory (){
+
+	public void HideStory() {
 		storyArea.setVisible(false);
 		deathCount.setVisible(false);
-		scroll.setVisible(false);	
+		scroll.setVisible(false);
 
 	}
-	
-	
+
 }
